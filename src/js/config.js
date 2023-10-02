7e4a39e5c1a639b1e1e4198b2934bc3a807f4392
nileshcode1 <itsoffcialcode@gmail.com>
2023-10-02 21:43:13 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '131818dc-dc87-47d0-ae7e-800a29e52f5b';
+export const MODAL_CLOSE_SEC = 2.5;
